c5f9ab3fad76541131f88534bb22450bdb08f1ed
Felipe <felipezb@live.com>
2018-12-29 23:16:41 +0200

    Added navigator, favorite names, changed theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,68 @@ void main() => runApp(new MyApp());
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  void _pushSaved() {
+    Navigator
+        .of(context)
+        .push(new MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return new Scaffold(
+        appBar: new AppBar(
+          title: const Text("Saved Suggestions"),
+        ),
+        body: _saved.length > 0 ? new ListView(
+          children: divided
+        ) : new Center(
+            child: Text("You didn't save any name")
+        )
+      );
+    }));
+  }
 
   Widget _buildSuggestions() {
     return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (BuildContext _context, int i ) {
-        if(i.isOdd) {
-          return new Divider();
-        }
-        final int index = i ~/ 2;
-        if(index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return new Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 
   Widget _buildRow(WordPair suggestion) {
+    final bool alreadySaved = _saved.contains(suggestion);
     return new ListTile(
-      title: new Text(suggestion.asPascalCase,
-      style: _biggerFont),
+      title: new Text(suggestion.asPascalCase, style: _biggerFont),
+      trailing: (new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      )),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(suggestion);
+          } else {
+            _saved.add(suggestion);
+          }
+        });
+      },
     );
   }
 
@@ -34,7 +74,10 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Startup name Generator")
+        title: new Text("Startup name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -51,6 +94,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.deepOrange,
+      ),
       home: RandomWords(),
     );
   }
